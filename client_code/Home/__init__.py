4515bd63d4ccce9f92de60d25fca63abd9befcb6
yarisingutarun@gmail.com <yarisingutarun@gmail.com>
2024-01-08 17:11:28 +0000

    Created LICENSE.txt and Home; Edited settings; Deleted Form1

--- client_code/Home/__init__.py
+++ client_code/Home/__init__.py
@@ -1,4 +1,4 @@
-from ._anvil_designer import Form1Template
+from ._anvil_designer import HomeTemplate
 from anvil import *
 import anvil.google.auth, anvil.google.drive
 from anvil.google.drive import app_files
@@ -8,7 +8,7 @@ from anvil.tables import app_tables
 import anvil.users
 import anvil.server
 
-class Form1(Form1Template):
+class Home(HomeTemplate):
   def __init__(self, **properties):
     # Set Form properties and Data Bindings.
     self.init_components(**properties)
