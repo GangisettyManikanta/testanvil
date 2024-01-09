b8f8b596b4239b49e5b53273e027fd297b09d9db
yarisingutarun@gmail.com <yarisingutarun@gmail.com>
2024-01-09 05:15:56 +0000

    Edited Home

--- client_code/Home/__init__.py
+++ client_code/Home/__init__.py
@@ -13,8 +13,6 @@ class Home(HomeTemplate):
     # Set Form properties and Data Bindings.
     self.init_components(**properties)
 
-    # Any code you write here will run before the form opens.
-
   def button_1_click(self, **event_args):
     name = anvil.server.call("name")
     self.label_2.text = name
